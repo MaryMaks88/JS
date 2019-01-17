87af91497fd5e6b4bc06b286eeeb62fd6a9d90ce
MaryMaks88 <8marymaks8@gmail.com>
2019-01-17 12:48:47 +0200

    L21_Upd

--- L21/L21_HW.js
+++ L21/L21_HW.js
@@ -1,41 +1,76 @@
-//-------------------
+// Lection -------------------
 
 window.onload = function(){
 
     var input = document.querySelector('.input');
     var innerText = [];
 
-    var inputTimeout = new Promise (function(resolve, reject){
+    new Promise (function(resolve, reject){
 
         function enterText(event){
 
-            // setTimeout(input.disabled = false, 1000);
-            
             if (event.keyCode == 13){
                 innerText.push(input.value);
-                input.value = ''; 
-                // input.disabled = true;
+                input.value = '';
+                input.disabled = true;    
             }
-  
+            setTimeout(() => {
+                input.disabled = false;
+            }, 2000);
         }
 
-        document.addEventListener('keypress', enterText)
+        document.addEventListener('keypress', enterText);
 
         setTimeout(() => { 
-           
-            if(innerText.length == 0){
-                reject();         
-            }
-
-            resolve();
-            
-        }, 10000)
-
-    })
-    
+            if(innerText.length == 0){ reject(); }
+            resolve();}, 10000)
+        }
+    )
         .then(
             () => {console.log(innerText);},
             () => {console.log('Array is empty');}
-    )
-          
+        )        
 }
+
+// Practice -------------------
+
+// var prom1 = new Promise(function(resolve, reject){
+//     setTimeout(() => {
+//         var number = Math.floor(Math.random()*100 + 0 );
+//         resolve('#1: ' + number);
+//     }, 1000);
+// });
+
+// var prom2 = new Promise(function(resolve, reject){
+//     setTimeout(() => {
+//         var number = Math.floor(Math.random()*100 + 0 );
+//         resolve('#2: ' + number);
+//     }, 2000);
+// });
+
+// var prom3 = new Promise(function(resolve, reject){
+//     setTimeout(() => {
+//         var number = Math.floor(Math.random()*100 + 0 );
+//         resolve('#3: ' + number);
+//     }, 3000);
+// });
+
+// function runAfter([func1, func2, func3]){
+//     func1();
+//     func2();
+//     func3();
+// }
+
+// Promise
+//     .all([prom1, prom2, prom3])
+//         .then(function(params){
+//             console.log(params);
+//         })
+//         .then(function(){
+//             runAfter([
+//                 () => {console.log('first one')},
+//                 () => {console.log('second one')},
+//                 () => {console.log('third one')}
+//             ]    
+//             );
+//         })
