0fb0734591dc8e8f64b216219130c7007680ed18
Mary <8marymaks8@gmail.com>
2019-01-21 21:49:22 +0200

    L22_node

--- L22_test/L22_HW.js
+++ L22_test/L22_HW.js
@@ -1,24 +1,72 @@
+// let result;
 
-let result;
+// doAjax('POST', '/data.json', "HELLO")
+//     .then(resp => {
+//         result = JSON.parse(resp);
+//         if(isNaN(result.age)){
+//             console.log('error age')
+//         } else{
+//             console.log(result.age)
+//         };
+        
+//         if (/\d/.test(result.name)) {
+//         console.log('error name')
+//         } else {
+//         console.log(result.name)
+//         };
+//         console.log(result);
+//     })
 
-doAjax('POST', '/data.json', "HELLO")
-    .then(resp => {
-        result = JSON.parse(resp);
-        console.log(result);
-    })
+// function doAjax(method, path, body){
+//     return new Promise(function(resolve, reject){
+//         var xhr = new XMLHttpRequest();
 
+//         xhr.onreadystatechange = function(){
+//             if(this.readyState != 4 || this.status != 200) return;
 
+//             resolve(this.responseText);
+//         }
+//         xhr.open(method, path);
+//         xhr.send(body);
+//     })
+// }
 
-function doAjax(method, path, body){
-    return new Promise(function(resolve, reject){
-        var xhr = new XMLHttpRequest();
+//------------------------
 
-        xhr.onreadystatechange = function(){
-            if(this.readyState != 4 || this.status != 200) return;
-
+let data1 = new Promise(function(resolve, reject){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(this.readyState != 4) return;
             resolve(this.responseText);
-        }
-        xhr.open(method, path);
-        xhr.send(body);
-    })
-}
+    }
+
+    xhr.open('GET', '/data1.json');
+
+    xhr.send();
+
+});
+
+let data2 = new Promise(function(resolve, reject){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(this.readyState != 4) return;
+            resolve(this.responseText);
+    }
+
+    xhr.open('GET', '/data2.json');
+
+    xhr.send();
+});
+
+Promise
+    .all([data1, data2])
+    .then(function(params){
+        for(var i = 0; i < params.length; i++){
+            var newArr1 = JSON.parse(params[0])
+            var newArr2 = JSON.parse(params[1])
+               
+        }   
+        console.log(newArr1.arr1, newArr2.arr2);
+        var res = newArr1.arr1.concat(newArr2.arr2);
+        console.log(res);
+                });
--- L22_test/server/server.js
+++ L22_test/server/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var static = require('node-static');
 var file = new static.Server('.');
-var port = 3898;
+var port = 3458;
 
 http.createServer(function(req, res) {
   file.serve(req, res);
